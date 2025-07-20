ab4e8d8c9b95dce7e3ab78ccd4798b9741ff6209
aminulwebdev <aminulwebdev@gmail.com>
2025-07-20 17:13:22 +0600

    Active path findout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Profile from "../assets/profile.jpg";
 import { GoHome } from "react-icons/go";
 import { LuMessageCircleMore } from "react-icons/lu";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { IoSettingsOutline } from "react-icons/io5";
 import { RiLogoutBoxRLine } from "react-icons/ri";
-import { Link, Links } from "react-router-dom";
+import { Link, Links, useLocation, useParams } from "react-router-dom";
 
 const Sidebar = () => {
+  // ============ React Router hook - path dhorar jonno ============
+  let location = useLocation();
+  let [activeValue, setActiveValue] = useState("");
+
+  // =============== useEffect For Active path ===============
+  useEffect(() => {
+    setActiveValue(location.pathname.replace("/pages/", "")); // ============ path ber korar jonno ""/pages/"" replace korar hoaech ========
+  });
+
   return (
     // ============= Profile Pic Here =============
     <div className="sidebar-layout">
@@ -17,16 +26,17 @@ const Sidebar = () => {
 
       {/* ============= Icon Here ============= */}
       <div className="page-layout">
-        <Link to="/pages/home" className="active">
+        <Link to="/pages/home" className={activeValue == "home" && "active"}>
+          {/* Short-circuit condition */}
           <GoHome className="page-icon" />
         </Link>
-        <Link to="/pages/message">
+        <Link to="/pages/message" className={activeValue == "message" && "active"}>
           <LuMessageCircleMore className="page-icon" />
         </Link>
-        <Link to="/pages/notification">
+        <Link to="/pages/notification" className={activeValue == "notification" && "active"}>
           <IoMdNotificationsOutline className="page-icon" />
         </Link>
-        <Link to="/pages/setting">
+        <Link to="/pages/setting" className={activeValue == "setting" && "active"}>
           <IoSettingsOutline className="page-icon" />
         </Link>
       </div>
