a0407e80762eb304faa1c70fdd0ab17c82a3e60b
aminulwebdev <aminulwebdev@gmail.com>
2025-07-22 20:36:30 +0600

    Forgot Password UI Added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,6 @@ import { RotatingLines } from "react-loader-spinner"; // ============ React load
 
 import { Link, useNavigate } from "react-router-dom"; // ============ Link, Navigate ============
 
-
 // =========== Text Field Customization =============
 
 const MyInput = styled(TextField)({
@@ -42,7 +41,7 @@ const MyButton = styled(Button)({
   width: "100%",
   borderRadius: "9px",
   height: "68px",
-  margin: "60px 0 35px",
+  margin: "60px 0 30px",
   fontSize: "20px",
   fontWeight: "600",
   textTransform: "none",
@@ -56,9 +55,13 @@ const Login = () => {
   let [input, setInput] = useState(""); //============= Password - Icon blank input field e Show/Hide useState ===============
   let [email, setEmail] = useState(""); //============= Email Show er jonno useState ===============
   let [password, setPassword] = useState(""); //============= Password er jonno useState ===============
+  let [forgotEmail, setForgotEmail] = useState(""); //============= Forget  Password - Email State ===============
 
   let [emailErr, setEmailErr] = useState(false); //============= Email Error Show korar jonno ===============
-  let [passwordErr, setpasswordErr] = useState(false);
+  let [passwordErr, setpasswordErr] = useState(false); //============= Password Error Show korar jonno ===============
+  let [forgotEmailErr, setForgotEmailErr] = useState(false); //============= Forget Password  - Email Error State ===============
+
+  let [forgotUI, setforgotUI] = useState(false); //============= Forget Password  UI ===============
 
   let [loader, setLoader] = useState(false); //============= React Loader er jonno useState ==============
   let navigate = useNavigate(); //============= sign up the login - navigate ==============
@@ -134,88 +137,140 @@ const Login = () => {
   let handleGoogle = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-      navigate("/pages/home")
+        navigate("/pages/home");
         console.log("google login");
-        
       })
       .catch((error) => {
         const errorCode = error.code;
         console.log(errorCode);
-        
       });
   };
 
+  //    ===========================================
+  //        New UI Forgot Password - Function
+  //   ===========================================
+
+  // ============= I forgot my password pera function ===============
+  let HandleforgetPassword = () => {
+    setforgotUI(true);
+  };
+  let handleBackLogin = () => {
+    setforgotUI(false);
+  };
+
+  let handleForgotEmail = (e) => {
+    setForgotEmail(e.target.value);
+    setForgotEmailErr("");
+  };
+
+  let handleSendButton = () => {
+    if (!forgotEmail) {
+      setForgotEmailErr("Email is required!");
+    } else {
+      if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        setForgotEmailErr("Please enter a valid email address.");
+      }
+    }
+  };
+
   return (
-    <Grid container>
-      <Grid size={6}>
-        <div className="login-content-box">
-          <div className="login-content">
-            <h2>Login to Flop Connect</h2>
-            {/* ===============  login with google ========== */}
-            <div onClick={handleGoogle} className="logo-box">
-              <img src={GoogleLogo} alt="" />
-              <span>Login with Google</span>
+    <>
+      {forgotUI ? (
+        // ============================================
+        //                 Forgot Password UI
+        //  =============================================
+        <div className="ui-for-forgotpassword">
+          <div className="forgotpassword-box-ui">
+            {/* ============= Email Input Field=========  */}
+            <div className="error-message-box ">
+              <MyInput value={forgotEmail} onChange={handleForgotEmail} id="outlined-basic" label="Email Address" variant="outlined" />
+              {forgotEmailErr && <p className="error-message">{forgotEmailErr}</p>}
             </div>
-            <div className="login-input-field">
-              {/* ============= Email Input Field========= */}
-              <div className="error-message-box">
-                <MyInput value={email} onChange={handleEmail} id="standard-basic" label="Email Address" variant="standard" />
-                {emailErr && <p className="error-message">{emailErr}</p>}
-              </div>
-              {/* ============= Password Input Field========= */}
-              <div className="password-input error-message-box">
-                <MyInput value={password} onChange={handlePassword} type={showPass ? "text" : "password"} id="standard-basic" label="Password" variant="standard" /> {/* Condition Apply */}
-                {/*========== onClick=()=>setShowPass(!showpass) - aivabe o lika jay - lada function na likhe ==========  */}
-                {input.trim() && (
-                  <div onClick={handleShowPassword} className="login-icon-box">
-                    {showPass ? <FiEye /> : <FiEyeOff />} {/* Condition Apply */}
-                  </div>
-                )}
-                {passwordErr && <p className="error-message">{passwordErr}</p>}
-              </div>
-            </div>
-
-            {/* ============= Login Button ========= */}
-            {/* ============= Loader customization ========= */}
-            {loader ? (
-              <div className="login-loader-icon">
-                <RotatingLines visible={true} height="50" width="50" color="grey" strokeWidth="5" animationDuration="0.4" ariaLabel="rotating-lines-loading" wrapperStyle={{}} wrapperClass="" />
-              </div>
-            ) : (
-              <MyButton onClick={handleLogin} className="signup-button" variant="contained">
-                Login to Continue
+            <div className="forget-button">
+              <MyButton onClick={handleBackLogin} className="signup-button" variant="contained">
+                Back to Login
               </MyButton>
-            )}
-
-            {/* ============= Toastify customization for Error message ========= */}
-            <ToastContainer
-              position="top-centre"
-              autoClose={2000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick={false}
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover={false}
-              theme="dark"
-              transition={Flip}
-            />
-
-            <p>
-              Don’t have an account ?{" "}
-              <Link to="/">
-                <span>Sign up & join the Flop!</span>
-              </Link>
-            </p>
+              <MyButton onClick={handleSendButton} className="signup-button" variant="contained">
+                Send
+              </MyButton>
+            </div>
           </div>
         </div>
-      </Grid>
-      {/* ============ Image Here =========== */}
-      <Grid size={6}>
-        <img className="reg-img" src={LoginImage} alt="" />
-      </Grid>
-    </Grid>
+      ) : (
+        <Grid container>
+          <Grid size={6}>
+            <div className="login-content-box">
+              <div className="login-content">
+                <h2>Login to Flop Connect</h2>
+                {/* ===============  login with google ========== */}
+                <div onClick={handleGoogle} className="logo-box">
+                  <img src={GoogleLogo} alt="" />
+                  <span>Login with Google</span>
+                </div>
+                <div className="login-input-field">
+                  {/* ============= Email Input Field========= */}
+                  <div className="error-message-box">
+                    <MyInput value={email} onChange={handleEmail} id="standard-basic" label="Email Address" variant="standard" />
+                    {emailErr && <p className="error-message">{emailErr}</p>}
+                  </div>
+                  {/* ============= Password Input Field========= */}
+                  <div className="password-input error-message-box">
+                    <MyInput value={password} onChange={handlePassword} type={showPass ? "text" : "password"} id="standard-basic" label="Password" variant="standard" /> {/* Condition Apply */}
+                    {/*========== onClick=()=>setShowPass(!showpass) - aivabe o lika jay - lada function na likhe ==========  */}
+                    {input.trim() && (
+                      <div onClick={handleShowPassword} className="login-icon-box">
+                        {showPass ? <FiEye /> : <FiEyeOff />} {/* Condition Apply */}
+                      </div>
+                    )}
+                    {passwordErr && <p className="error-message">{passwordErr}</p>}
+                  </div>
+                </div>
+                {/* ============= Login Button ========= */}
+                {/* ============= Loader customization ========= */}
+                {loader ? (
+                  <div className="login-loader-icon">
+                    <RotatingLines visible={true} height="50" width="50" color="grey" strokeWidth="5" animationDuration="0.4" ariaLabel="rotating-lines-loading" wrapperStyle={{}} wrapperClass="" />
+                  </div>
+                ) : (
+                  <MyButton onClick={handleLogin} className="signup-button" variant="contained">
+                    Login to Continue
+                  </MyButton>
+                )}
+                {/* ============= Toastify customization for Error message ========= */}
+                <ToastContainer
+                  position="top-centre"
+                  autoClose={2000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick={false}
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover={false}
+                  theme="dark"
+                  transition={Flip}
+                />
+
+                {/* ============== Forget Password ============== */}
+                <h3 onClick={HandleforgetPassword} className="forget-password">
+                  I forgot my password
+                </h3>
+                <p>
+                  Don’t have an account ?{" "}
+                  <Link to="/">
+                    <span>Sign up & join the Flop!</span>
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </Grid>
+          {/* ============ Image Here =========== */}
+          <Grid size={6}>
+            <img className="reg-img" src={LoginImage} alt="" />
+          </Grid>
+        </Grid>
+      )}
+    </>
   );
 };
 
